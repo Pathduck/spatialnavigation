bbc18f90de063656ba76ae6c1cb014984e6d39e7
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2021-03-10 00:52:18 -0700

    Background script for setting default options on install

---
+++ js/background.js
@@ -0,0 +1,41 @@
+chrome.runtime.onInstalled.addListener(function () {
+  let settings = {
+    hotkeys: {
+      codes: {
+        slower: ["F21"],
+        normal: ["F22"],
+        faster: ["F23"],
+        pause: ["k"],
+        "skip-back": ["ArrowLeft"],
+        "skip-forward": ["ArrowRight"],
+      },
+    },
+  };
+
+  chrome.storage.local.set({ "extension-settings": settings }, function () {
+    console.log("Initial setting set...");
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.from === "content" && msg.subject === "changeIcon") {
+    // let canvas = document.getElementById("icon-making-canvas");
+    // if (!canvas) {
+    //   canvas = document.createElement("CANVAS");
+    //   canvas.id = "icon-making-canvas";
+    //   canvas.width = canvas.height = 16;
+    // }
+    // let ctx = canvas.getContext("2d");
+    // ctx.beginPath();
+    // ctx.rect(0, 0, 16, 16);
+    // ctx.fillStyle = "red";
+    // ctx.fill();
+    // ctx.fillStyle = "white";
+    // ctx.textAlign = "center";
+    // ctx.textBaseline = "middle";
+    // fitTextOnCanvas(ctx, msg.speed, "Roboto Condensed", 9, canvas.width);
+    // let imgData = ctx.getImageData(0, 0, 16, 16);
+    // chrome.pageAction.setIcon({ imageData: imgData, tabId: sender.tab.id });
+  }
+  return Promise.resolve("Dummy response to keep the console quiet");
+});
